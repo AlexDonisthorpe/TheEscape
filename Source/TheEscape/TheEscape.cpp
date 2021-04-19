59df7c922cea7129912abbf5ac42722889337d40
Alex Donisthorpe <alex.donisthorpe+doneecans@gmail.com>
2021-04-19 16:05:15 +0100

    Initiliase the repo

---
+++ Source/TheEscape/TheEscape.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TheEscape.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TheEscape, "TheEscape" );
---
+++ Source/TheEscape/TheEscapeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TheEscapeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THEESCAPE_API ATheEscapeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
